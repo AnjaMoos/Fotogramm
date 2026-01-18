d98a6f0a03f8e6d7a3fedcbf1b3cde9ca2027e51
AnjaMoos <anja.moosbuehler@web.de>
2026-01-18 21:44:30 +0100

    18.01.2026

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+let currentIndex = 0;
+
 // Template Funktionen
 
-let randomTitles = [];
+let titles = ["Enrntereifes Gerstenfeld",
+    "Sonne nach dem Regen über dem Weizenfeld",
+    "Atemberaubendes Seeufer",
+    "Schwarzer Schwan im Stausee",
+    "Kreative Wohnung",
+    "Burg Trausniz in Landshut",
+    "Peter Fox Konzert in Wien 2024",
+    "Salzgrotte in Landshut",
+    "Frühstück in Basel",
+    "Wake and Groove in Geisenfeld",
+    "Ich im Pelzmantel",
+    "Winterlandschaft"
 
-let randomDescriptions = [];
+];
 
 let myImages = [
     "IMG20240710102643.jpg",
@@ -26,21 +39,71 @@ function render() {
     }
 }
 
-function getNotesHtml(index){
- return `
- <img class="front_photo" src='./img/dialog/${myImages[index]}' alt='private photo'>
+function getNotesHtml(index) {
+    return `
+ <img onclick="openDialog(${index})" class="front_photo" src='./img/dialog/${myImages[index]}' alt='private photo'>
  `
 }
 
 // Dialog
 
-function openDialog() {
+function openDialog(index) {
+currentIndex = index;
+
+    let dialogRef = document.getElementById('myDialog');
+    let imageRef = document.getElementById('dialogImage');
+    let titleRef = document.getElementById('dialogTitle');
+
+
+    titleRef.innerText = titles[currentIndex];
+
+
+    imageRef.innerHTML = `
+        <img class="dialog_img" src="./img/dialog/${myImages[currentIndex]}" alt="private photo">
+    `;
+
+
     dialogRef.showModal();
     dialogRef.classList.add("opened");
 }
 
 function closeDialog() {
+    let dialogRef = document.getElementById('myDialog')
     dialogRef.close();
     dialogRef.classList.remove("opened");
     dialogRef.classList.add("closed")
+}
+
+function nextPicture(){
+       currentIndex++;
+
+    if (currentIndex >= myImages.length) {
+        currentIndex = 0; 
+    }
+
+    updateDialogImage();
+}
+
+
+function pictureBackwards(){
+      currentIndex--;
+
+    if (currentIndex < 0) {
+        currentIndex = myImages.length - 1; // zum letzten Bild
+    }
+
+    updateDialogImage();
+}
+
+function updateDialogImage() {
+    let imageRef = document.getElementById('dialogImage');
+    let titleRef = document.getElementById('dialogTitle');
+
+    titleRef.innerText = titles[currentIndex];
+
+    imageRef.innerHTML = `
+        <img class="dialog_img"
+             src="./img/dialog/${myImages[currentIndex]}"
+             alt="private photo">
+    `;
 }
